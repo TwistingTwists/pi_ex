c78cdce2dcf47b66fa9ec6ba41dd1895c86ace6c
Abhishek Tripathi <abhishek.tripathi456@gmail.com>
2026-04-22 23:59:08 +0530

    chore: bump version for publish

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PiEx.MixProject do
   def project do
     [
       app: :pi_ex_native,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
